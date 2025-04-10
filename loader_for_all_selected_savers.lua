6a8959b10602dfacccf8079dc1aa285d3f96b222
SJAAHEIM <realcharredapps@icloud.com>
2025-04-10 13:17:23 -0500

    adding loader for all selected savers script

---
+++ loader_for_all_selected_savers.lua
@@ -0,0 +1,36 @@
+fusion = Fusion()
+fu = fusion
+composition = fu.CurrentComp
+comp = composition
+SetActiveComp(comp)
+
+comp:Lock()
+
+local selectedSavers = comp:GetToolList(true, "Saver")
+
+if #selectedSavers == 0 then
+    print("No Savers selected")
+else
+    -- track pos for new loaders
+    local loaderX = 0
+    local loaderY = 1
+    -- get flow view for positioning 
+    local flow = comp.CurrentFrame.FlowView
+    -- create a loader for each selected saver
+    for i, saver in pairs(selectedSavers) do
+        print("Creating Loader for " .. saver.Name)
+        local saverClipPath = saver:GetInput("Clip")
+        if saverClipPath and saverClipPath ~= "" then
+            local saverX, saverY = flow:GetPos(saver)
+            -- position the loader to the right of the saver
+            local loader = comp:AddTool("Loader", saverX, saverY + loaderY)
+            -- set loaders path to the saver clip path
+            loader:SetInput("Clip", saverClipPath)
+            -- name the loader based on saver 
+            local 
+end
+
+
+comp:Unlock()
+
+print("All Selected Savers now have Loaders")
